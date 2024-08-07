88a68ea50e1bc785516761ae58dd2c58da13af25
Roman <133262465+Roman12345678900@users.noreply.github.com>
2024-08-07 23:10:19 +0300

    Add files via upload

--- changelogs/homework-2/sql/13.create-reviews-table.sql
+++ changelogs/homework-2/sql/13.create-reviews-table.sql
@@ -1,7 +1,7 @@
 create table reviews (
     id bigserial primary key not null,
     rating int not null,
-    review_text text not null,
+    review_text varchar(500) not null,
     review_date date not null,
     class_id bigint not null,
     user_id bigint not null,
