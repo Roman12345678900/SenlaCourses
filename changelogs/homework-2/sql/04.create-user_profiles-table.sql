54a87db73a573aa4ef54284e6048ddc677a6c7bd
Roman <133262465+Roman12345678900@users.noreply.github.com>
2024-08-09 21:10:48 +0300

    Add files via upload

--- changelogs/homework-2/sql/04.create-user_profiles-table.sql
+++ changelogs/homework-2/sql/04.create-user_profiles-table.sql
@@ -1,10 +1,10 @@
 create table profiles (
     id bigserial primary key not null,
     user_id bigint unique not null,
-    address varchar(100),
-    date_of_birth date,
-    gender varchar(30),
-    phone varchar(20),
+    address varchar(100) default null,
+    date_of_birth date default null,
+    gender varchar(30) default null,
+    phone varchar(20) default null,
     foreign key (user_id) references users(id)
 );
 
--- changelogs/homework-2/sql/12.create-schedules-table.sql
+++ changelogs/homework-2/sql/12.create-schedules-table.sql
@@ -4,7 +4,7 @@ create table schedules (
     type varchar(20) not null,
     start_time timestamp not null,
     end_time timestamp not null,
-    schedule_id bigint not null,
+    schedule_id bigint,
     class_id bigint not null,
     foreign key (schedule_id) references schedules(id),
     foreign key (class_id) references classes(id)
